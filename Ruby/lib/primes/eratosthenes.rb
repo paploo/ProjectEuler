a26bc1fd5813571289349835c2390fe6d8b381f9
Jeff Reinecke <jeff@paploo.net>
2011-08-18 19:40:17 -0700

    Ruby/Lib/Eratosthenes: Re-did some comments and abstracted a bit.

--- Ruby/lib/primes/eratosthenes.rb
+++ Ruby/lib/primes/eratosthenes.rb
@@ -1,25 +1,31 @@
 module Primes
-  # Sieve of Eratosthenes
+  # Sieve of Eratosthenes for generation of a list of prime numbers.
   module Eratosthenes
-  
-  # Generate a list of all primes up to the given number.
-  # Note that this makes an array big enough to hold all
-  # the values to test.
-    def self.primes(n=1024)
+    
+    # The primary prime generation method for this module.
+    #
+    # This allows for switching between multiple implementations.
+    def self.generate_primes(n=1024)
+      def generate_primes_basic(n)
+    end
+    
+    # This is the most straight-forward implementation, but is incredibly RAM
+    # inefficient.
+    def self.generate_primes_basic(n=1024)
       # First, we need to make the Array.
       # To make it easy later, we set the values to the index.
       # Note that this isn't meaningfully different in RAM usage or time than
       # only using booleans.
       list = Array.new(n+1) {|i| i}
-    
+      
       # Initialize some values that we don't want in our list later.
       list[0] = false
       list[1] = false
-    
+      
       # Try building up the multiples of all values.
       x = 2
       while(x*x <= n)
-      
+        
         # If we reach a value that is not marked as not prime, it is prime, and
         # we need to mark all its multiples as not prime.
         if( list[x] != false )
@@ -27,7 +33,7 @@ module Primes
             list[a*x] = false
           end
         end
-      
+        
         x += 1
       end
       
